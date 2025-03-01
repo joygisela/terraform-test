ac10bfac39b567f48d0d82f49a336303bf34134e
joygisela <fabricenguenouho87@gmail.com>
2025-03-01 17:48:48 -0600

    terraform code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create a new GitLab Lightsail Instance
 resource "aws_lightsail_instance" "gitlab_test" {
-  name              = "custom_gitlab"
+  name              = "myserver"
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_3_0"
--- version.tf
+++ version.tf
@@ -1,11 +1,15 @@
 terraform {
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
 
   }
   #This line is inactive
   
 }
 provider "aws" {
+  region = "us-east-1"
 
 }
